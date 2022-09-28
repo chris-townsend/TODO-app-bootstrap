dfc32b4ed4880592ee234da477f3aa9406c86dea
chris-townsend <chris-townsend@live.co.uk>
2022-09-28 15:19:35 +0000

    Add js function to create task and reset

--- main.js
+++ main.js
@@ -21,22 +21,50 @@ let formValidation = () => {
       add.click();
   
       (() => {
-        add.setAttribute("data-bs-dismiss", "");
+      add.setAttribute("data-bs-dismiss", "");
       })();
     }
   };
 
-let data = [];
+
+  let data = [];
 
 let acceptData = () => {
-      data.push({
-      text: textInput.value,
-      date: dateInput.value,
-      description: textarea.value,
-    });
-  
-    localStorage.setItem("data", JSON.stringify(data));
-  
-    console.log(data);
-  };
+  data.push({
+    text: textInput.value,
+    date: dateInput.value,
+    description: textarea.value,
+  })
+  createTasks();
+  ;
 
+  localStorage.setItem("data", JSON.stringify(data));
+
+  console.log(data);
+};
+
+let createTasks = () => {
+  tasks.innerHTML = "";
+  data.map((x, y) => {
+    return (tasks.innerHTML += `
+    <div id=${y}>
+          <span class="fw-bold">${x.text}</span>
+          <span class="small text-secondary">${x.date}</span>
+          <p>${x.description}</p>
+  
+          <span class="options">
+            <i onClick= "editTask(this)" data-bs-toggle="modal" data-bs-target="#form" class="fas fa-edit"></i>
+            <i onClick ="deleteTask(this);createTasks()" class="fas fa-trash-alt"></i>
+          </span>
+        </div>
+    `);
+  });
+
+  resetForm();
+};
+
+let resetForm = () => {
+  textInput.value = "";
+  dateInput.value = "";
+  textarea.value = "";
+};
