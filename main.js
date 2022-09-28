2470378c54c8c89474456dc74cf77910976eec2c
chris-townsend <chris-townsend@live.co.uk>
2022-09-28 16:20:55 +0000

    Add edit task function

--- main.js
+++ main.js
@@ -79,3 +79,12 @@ let deleteTask = (e) => {
   console.log(data);
 };
 
+let editTask = (e) => {
+  let selectedTask = e.parentElement.parentElement;
+
+  textInput.value = selectedTask.children[0].innerHTML;
+  dateInput.value = selectedTask.children[1].innerHTML;
+  textarea.value = selectedTask.children[2].innerHTML;
+
+  deleteTask(e);
+};
